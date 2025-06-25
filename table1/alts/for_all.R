a60f319fea6acd7a12994f4acc9ab53ab8808fa9
IoannisPapageorgiou <32958138+IoannisPapageorgiou@users.noreply.github.com>
2025-06-25 17:22:53 +0100

    Add files via upload

---
+++ table1/alts/for_all.R
@@ -0,0 +1,27 @@
+mse = 0;
+
+xn = read.csv("sim1.txt", header = FALSE) ##read desired dataset here
+n = length(xn[,1])
+n2 = n/2
+
+for (i in n2:n){
+  
+  #update model every time-step
+  temp = xn[1:(i-1),1];
+  dts = ts(temp);
+  ar = auto.arima(dts);  ## this is for ARIMA model
+  #ar = ets(dts); ## this is for ETS model
+  #ar = nnetar(dts,p=2); ## this is for NNAR model
+
+  f = forecast(ar);
+  p  = unname(unlist(f["mean"]));
+  er = p[1]-xn[i,1];
+
+  mse = mse + er^2
+}
+
+mse / n2
+
+
+
+
