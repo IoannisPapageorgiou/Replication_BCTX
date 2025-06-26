b611d8f147460c26fb7863d1e4e05805dbe4c6f7
IoannisPapageorgiou <32958138+IoannisPapageorgiou@users.noreply.github.com>
2025-06-26 03:07:02 +0100

    Update all_garch_pred.R

--- table2/alts/all_garch_pred.R
+++ table2/alts/all_garch_pred.R
@@ -1,15 +1,12 @@
 library(rugarch)
 
 #tf is data
-tf=read.delim("ftse.txt", header = FALSE) #can similarly replace this with other datasets
-
+tf=read.delim("ftse.txt", header = FALSE) # can similarly replace this with other datasets
 
 mysize=nrow(tf);
 train=nrow(tf)-130+1;
 
-
 ll=0
-
 
 specg = ugarchspec(variance.model = list(model="sGARCH"), mean.model = list(armaOrder = c(0,0), include.mean = FALSE)) ## GARCH model
 #specg = ugarchspec(variance.model = list(model="gjrGARCH"), mean.model = list(armaOrder = c(0,0), include.mean = FALSE)) ## GJR model
@@ -23,7 +20,6 @@ for (i in train:mysize){
   forc = ugarchforecast(fit, n.ahead=1)
   pr = as.numeric(forc@forecast["sigmaFor"])
   #prm=as.numeric(forc@forecast["seriesFor"])
-  
   
   ll= ll + dnorm(tf[i,1], mean =0, sd = pr, log = TRUE)
   
