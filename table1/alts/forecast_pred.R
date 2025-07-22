a3a5389210db2cbd27eb06d06c5678c8646c28e9
IoannisPapageorgiou <32958138+IoannisPapageorgiou@users.noreply.github.com>
2025-07-22 20:26:40 +0100

    Update forecast_pred.R

--- table1/alts/forecast_pred.R
+++ table1/alts/forecast_pred.R
@@ -2,7 +2,7 @@ library(forecast)
 
 mse = 0;
 
-xn = read.csv("sim1.txt", header = FALSE) ## read desired dataset; here sim1 is used, but can similarly use with the other datasets.
+xn = read.csv("../data/sim1.txt", header = FALSE) ## read desired dataset; here sim1 is used, but can similarly use with the other datasets.
 n = length(xn[,1])
 n2 = n/2
 
