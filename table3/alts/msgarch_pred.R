092383f92d4d2b3700fd99ebee8f4c7c91549036
IoannisPapageorgiou <32958138+IoannisPapageorgiou@users.noreply.github.com>
2025-07-22 20:42:31 +0100

    Add files via upload

---
+++ table3/alts/msgarch_pred.R
@@ -0,0 +1,36 @@
+library(MSGARCH)
+
+tf=read.delim("../data/ftse.txt", header = FALSE) # read data
+tfnum=as.numeric(unlist(tf))
+
+
+mysize=nrow(tf);
+train=nrow(tf)-130+1;
+
+
+ll=0
+
+
+
+spec_ms <- CreateSpec(variance.spec = list(model = c("sGARCH", "sGARCH")),distribution.spec = list(distribution = c("norm", "norm")))
+#spec_ms <- CreateSpec(variance.spec = list(model = c("sGARCH", "sGARCH", "sGARCH")),distribution.spec = list(distribution = c("norm", "norm", "norm")))
+
+for (i in train:mysize){
+  
+  
+  #print(i)
+  y22 = tfnum[1:i-1];
+  
+  
+  fitms = FitML(spec = spec_ms, data =y22)
+  
+  
+  prv= as.numeric(unlist(predict(fitms, nahead=1)))
+  
+  
+  ll= ll + dnorm(tf[i,1], mean = 0, sd = prv, log = TRUE)
+  
+}
+
+
+ll
